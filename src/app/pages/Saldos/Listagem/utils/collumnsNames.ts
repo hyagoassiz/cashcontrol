5c19e04cc2b1349f8e8c34487ad5d79571d4935c
hyagoassiz <hyagoassiz@hotmail.com>
2024-07-26 12:05:56 -0300

    CCP-15 refactor(Saldos): listando valores na tabela

---
+++ src/app/pages/Saldos/Listagem/utils/collumnsNames.ts
@@ -0,0 +1,7 @@
+export const COLLUMS_SALDO = [
+  { key: "conta", label: "Conta" },
+  { key: "entrada", label: "Entradas" },
+  { key: "saida", label: "Saídas" },
+  { key: "saldo", label: "Saldo Atual" },
+  { key: "options", label: "", style: { width: "10px" } },
+];
